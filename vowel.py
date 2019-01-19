4a29a79f836d59e8744efc79cfa3ac081a413a88
beenujb <41043473+beenujb@users.noreply.github.com>
2019-01-19 14:53:55 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,9 @@
+ch = input("Enter any character: ");
+if ch == '0':
+    exit();
+else:
+    if(ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I'
+       or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
+    	    print(ch, "is a vowel.");
+    else:
+    	print( "yes");
