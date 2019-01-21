7c3579c6653102a46fa989e7579ccc154fc178fc
beenujb <41043473+beenujb@users.noreply.github.com>
2019-01-20 23:13:38 -0800

    Create composite.py

---
+++ composite.py
@@ -0,0 +1,12 @@
+num = 123
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print( "yes")
+            break
+    else:
+        print(num, "is a PRIME number")
+elif num == 0 or 1:
+    print(num, "is a neither prime NOR composite number")
+else:
+    print(num, "is NOT a prime number it is a COMPOSITE number")
