2036dded17551a5e7f845c6fd716e11cb8fbad18
beenujb <41043473+beenujb@users.noreply.github.com>
2019-01-20 23:52:54 -0800

    Create multiple.py

---
+++ multiple.py
@@ -0,0 +1,42 @@
+# Python profram to check if n is a multiple of 13 
+  
+# Function to check if n is a multiple of 13 
+def isMultipleOf13(n): 
+  
+    odd_count = 0
+    even_count = 0
+  
+    # Make no positive if +n is multiple of 3 
+    # then is -n. We are doing this to avoid 
+    # stack overflow in recursion 
+    if(n < 0):  
+        n = -n 
+    if(n == 0): 
+        return 1
+    if(n == 1):  
+        return 0
+  
+    while(n): 
+          
+        # If odd bit is set then 
+        # increment odd counter  
+        if(n & 1):  
+            odd_count += 1
+        n = n >> 1
+  
+        # If even bit is set then 
+        # increment even counter  
+        if(n & 1): 
+            even_count += 1
+        n = n >> 1
+  
+    return isMultipleOf13(abs(odd_count - even_count)) 
+  
+# Program to test function isMultipleOf3  
+num = 26
+if (isMultipleOf13(num)):  
+    print(num, 'is multiple of 13') 
+else: 
+    print('yes') 
+  
+# This code is contributed by Danish Raza 
